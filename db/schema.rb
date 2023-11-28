3e6ef39b07a3f5cbf0da89d9cae0f8125e59e2fa
Abdurrehman0024 <abd.rehman.0024@gmail.com>
2023-11-28 11:52:22 +0000

    add user password_hash

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_11_24_122034) do
+ActiveRecord::Schema.define(version: 2023_11_27_161120) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -21,10 +21,11 @@ ActiveRecord::Schema.define(version: 2023_11_24_122034) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "username"
+    t.string "email"
+    t.string "password_digest"
   end
 
 end
